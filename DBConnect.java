44517261668a8a5ea6962d5ebdec1a204621d91d
Devop-Coder <123042504+Devop-Coder@users.noreply.github.com>
2023-01-19 01:24:05 -0500

    added prayer

--- DBConnect.java
+++ DBConnect.java
@@ -3,3 +3,7 @@ do not get discourage when you thing you made mistake
 you are not in competition with anyone
 God will always give you speed , okay franca
 Thanks 
+O lord trouble my troubler
+give my troubler no rest
+rise a defend for us
+Amen
